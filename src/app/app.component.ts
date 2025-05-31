961e1cc496fb2e14c8ce826d1be432e8949b8f19
Somasundar <somasundar.work@outlook.com>
2025-06-01 01:21:30 +0530

    Remove Bootstrap dependency and styles, add About component with its template, styles, and tests, and update AppComponent to include new components for a structured layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SummaryComponent } from './Pages/summary/summary.component';
+import { EducationComponent } from './Pages/education/education.component';
+import { WorkComponent } from './Pages/work/work.component';
+import { ProjectsComponent } from './Pages/projects/projects.component';
+import { SkillsComponent } from './Pages/skills/skills.component';
+import { AboutComponent } from './Pages/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    SummaryComponent,
+    EducationComponent,
+    WorkComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cv';
